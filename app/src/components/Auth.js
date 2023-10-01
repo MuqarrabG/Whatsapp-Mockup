1f9d961cd37802c0f0d1b1340c8da29fd2c2d49e
MuqarrabG <84062956+MuqarrabG@users.noreply.github.com>
2023-10-02 02:31:27 +1100

    Sign up page basic functionality

---
+++ app/src/components/Auth.js
@@ -0,0 +1,37 @@
+// src/LoginPage.js
+import React, { useState } from 'react';
+
+function LoginPage() {
+  const [credentials, setCredentials] = useState({ email: '', password: '' });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCredentials(prevState => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: Authenticate the user using an API
+    console.log(credentials);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label class="block">Email:</label>
+          <input type="email" name="email" value={credentials.email} onChange={handleChange} required />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" name="password" value={credentials.password} onChange={handleChange} required />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
