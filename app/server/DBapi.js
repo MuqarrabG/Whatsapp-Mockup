1303ed0c13e8004c1d2f13ae746795538ddda1f6
Werdna6001 <andrew.adams1@students.mq.edu.au>
2023-10-10 17:40:55 +1100

    added post requests

--- app/server/DBapi.js
+++ app/server/DBapi.js
@@ -31,7 +31,6 @@ DataBase.get('/db/users/:user', (req, res) => {
 
 DataBase.post('/db/users', (req, res) => {
     const body = req.body
-    console.log(body)
     const newUser = {
         userId: data.nextUserId,
         username: body.username,
@@ -48,16 +47,16 @@ DataBase.post('/db/users', (req, res) => {
     })
 })
 
-DataBase.get(['/db/chats','/db/groups'], (req, res) => {
-    res.json(data.chats)
+DataBase.get('/db/groups', (req, res) => {
+    res.json(data.groups)
 })
 
-DataBase.get(['/db/chats/:chat','/db/groups/:chat'], (req, res) => {
-    const chat = req.params.chat
-    const chats = data.chats
+DataBase.get('/db/groups/:group', (req, res) => {
+    const group = req.params.group
+    const groups = data.groups
     let found = false
-    chats.map(c => {
-        if(c.groupId===Number(chat)){
+    groups.map(c => {
+        if(c.groupId===Number(group)){
             found = true
             res.json(c)
         }
@@ -65,22 +64,137 @@ DataBase.get(['/db/chats/:chat','/db/groups/:chat'], (req, res) => {
     if(!found)res.status(401).json({error: "No group with that ID exists"})
 })
 
-DataBase.get(['/db/:user/chats','/db/:user/groups'], (req, res) => {
+DataBase.post('/db/groups', (req, res) => {
+    const body = req.body
+    const newGroup = {
+        groupId: data.nextGroupId,
+        nextMessageId: 0,
+        messages: [],
+        mambers: [
+            {
+                id: body.founderId,
+                nickname: body.founderName
+            }
+        ]
+    }
+    data.groups.push(newGroup)
+    data.nextGroupId+=1
+    writeFile(data, (error) => {
+        if(error){
+            res.status(404).send("Group not Saved")
+            return
+        }
+        res.send("Group Saved")
+    })
+})
+
+DataBase.get(['/db/:user/groups'], (req, res) => {
     const user = req.params.user
-    const chats = data.chats
-    let usersChats = []
-    chats.map(c => {
+    const groups = data.groups
+    let usersgroups = []
+    groups.map(c => {
         c.members.map(m => {
             if(m.id===Number(user)){
-                usersChats.push(c)
+                usersgroups.push(c)
             }
         })
     })
-    if(usersChats.length===0){
+    if(usersgroups.length===0){
         res.status(401).json({error: "This user has no groups yet."})
     }else {
-        res.json(usersChats)
+        res.json(usersgroups)
     }
+})
+
+DataBase.post('/db/groups/:group/invite', (req, res) => {
+    const body = req.body
+    const group = req.params.group
+    let found = false
+    const newMember = {
+        id: body.memberId,
+        nickname: body.memberName
+    }
+    data.groups.map((g) => {
+        if(g.groupId===Number(group)){
+            found = true
+            g.members.push(newMember)
+        }
+    })
+    if(!found)res.status(401).json({error: "No group with that ID exists"})
+    writeFile(data, (error) => {
+        if(error){
+            res.status(404).send("Group not Updated")
+            return
+        }
+        res.send("Group Updated")
+    })
+})
+
+DataBase.post('/db/groups/:group/post', (req, res) => {
+    const body = req.body
+    const group = req.params.group
+    let found = false
+    data.groups.map((g) => {
+        if(g.groupId===Number(group)){
+            found = true
+            const newMessage = {
+                messageId: g.nextMessageId,
+                content: body.content,
+                author: body.authorId,
+                reactions: []
+            }
+            g.messages.push(newMessage)
+        }
+    })
+    if(!found)res.status(401).json({error: "No group with that ID exists"})
+    writeFile(data, (error) => {
+        if(error){
+            res.status(404).send("Message not Sent")
+            return
+        }
+        res.send("Message Sent")
+    })
+})
+
+DataBase.post('/db/groups/:group/:message/react', (req, res) => {
+    const body = req.body
+    const group = req.params.group
+    const message = req.params.message
+    let foundGroup = false
+    let foundMessage = false
+    data.groups.map((g) => {
+        if(g.groupId===Number(group)){
+            foundGroup = true
+            g.messages.map((m) => {
+                if(m.messageId===Number(message)){
+                    foundMessage = true
+                    m.reactions.map((r) => {
+                        if(body.authorId===r.author){
+                            if(body.icon===r.icon){
+                                //delete reaction
+                            }else{
+                                //re-label reaction
+                            }
+                        }
+                    })
+                    const newReaction = {
+                        icon: body.icon,
+                        author: body.authorId
+                    }
+                    m.reactions.push(newReaction)
+                }
+            })
+        }
+    })
+    if(!foundGroup)res.status(401).json({error: "No group with that ID exists"})
+    if(!foundMessage)res.status(401).json({error: "No message with that ID exists"})
+    writeFile(data, (error) => {
+        if(error){
+            res.status(404).send("Message not Sent")
+            return
+        }
+        res.send("Message Sent")
+    })
 })
 
 module.exports = DataBase
--- app/server/calls.md
+++ app/server/calls.md
@@ -15,21 +15,67 @@
         - calls to this may need to be modified
     - returns the data for a specific user
 
-## GET /db/chats || GET /db/groups
+## GET /db/groups
     - development only
         - don't leave a call to this in the finished product
     - returns all the group data for every group
 
-## GET /db/chats/:chat || GET /db/groups/:chat
+## GET /db/groups/:chat
     - authentication to be added
         - calls to this may need to be modified
     - returns the data for a specific group
 
-## GET /db/:userId/chats || GET /db/:userId/groups
+## GET /db/:userId/groups
     - authentication to be added
         - calls to this may need to be modified
     - returns all the data for every group a specific user is in
 
 ## POST /db/users
     - adds a new user to the list of users
-    - body must be a JSON object with the username and password of the new user in text.
+    - body must be a JSON object with the format:
+    {
+        username: (String),
+        password: (String)
+    }
+
+## POST /db/groups
+    - adds a new group to the list of groups
+    - body must be a JSON object with the format:
+    {
+        founderId: (Int),
+        founderName: (String)
+    }
+    - untested
+        - contact me if you have an error with it
+
+## POST /db/groups/:groupId/invite
+    - adds a new member to a specific group
+    - body must be a JSON object with the format:
+    {
+        memberId: (Int),
+        memberName: (String)
+    }
+    - untested
+        - contact me if you have an error with it
+
+## POST /db/groups/:groupId/post
+    - adds a new member to a specific group
+    - body must be a JSON object with the format:
+    {
+        content: (String),
+        authorId: (Int)
+    }
+    - untested
+        - contact me if you have an error with it
+
+## POST /db/groups/:groupId/:messageId/react
+    - adds a new member to a specific group
+    - body must be a JSON object with the format:
+    {
+        icon: (String),
+        authorId: (Int)
+    }
+    - incomplete
+        - will talk about the extent of this feature soon
+    - untested
+        - contact me if you have an error with it
