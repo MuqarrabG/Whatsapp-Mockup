24c86b41f3233fe7e8ad71eef50f2ff8020bbd08
Werdna6001 <andrew.adams1@students.mq.edu.au>
2023-10-12 10:50:07 +1100

    Added reaction calls to the backend

--- app/server/DBapi.js
+++ app/server/DBapi.js
@@ -11,11 +11,9 @@ const writeFile = (newData, returnFunction) => {
 DataBase.get('/db/', (req, res) => {
     res.json(data)
 })
-
 DataBase.get('/db/users', (req, res) => {
     res.json(data.users)
 })
-
 DataBase.get('/db/users/:user', (req, res) => {
     const user = req.params.user
     const users = data.users
@@ -28,7 +26,6 @@ DataBase.get('/db/users/:user', (req, res) => {
     })
     if(!found)res.status(401).json({error: "No user with that ID exists"})
 })
-
 DataBase.post('/db/users', (req, res) => {
     const body = req.body
     const newUser = {
@@ -46,11 +43,9 @@ DataBase.post('/db/users', (req, res) => {
         res.send("User Saved")
     })
 })
-
 DataBase.get('/db/groups', (req, res) => {
     res.json(data.groups)
 })
-
 DataBase.get('/db/groups/:group', (req, res) => {
     const group = req.params.group
     const groups = data.groups
@@ -63,7 +58,6 @@ DataBase.get('/db/groups/:group', (req, res) => {
     })
     if(!found)res.status(401).json({error: "No group with that ID exists"})
 })
-
 DataBase.post('/db/groups', (req, res) => {
     const body = req.body
     const newGroup = {
@@ -87,7 +81,6 @@ DataBase.post('/db/groups', (req, res) => {
         res.send("Group Saved")
     })
 })
-
 DataBase.get(['/db/:user/groups'], (req, res) => {
     const user = req.params.user
     const groups = data.groups
@@ -105,7 +98,6 @@ DataBase.get(['/db/:user/groups'], (req, res) => {
         res.json(usersgroups)
     }
 })
-
 DataBase.post('/db/groups/:group/invite', (req, res) => {
     const body = req.body
     const group = req.params.group
@@ -129,7 +121,6 @@ DataBase.post('/db/groups/:group/invite', (req, res) => {
         res.send("Group Updated")
     })
 })
-
 DataBase.post('/db/groups/:group/post', (req, res) => {
     const body = req.body
     const group = req.params.group
@@ -155,46 +146,165 @@ DataBase.post('/db/groups/:group/post', (req, res) => {
         res.send("Message Sent")
     })
 })
-
+DataBase.get('/db/groups/:group/:message/react/:userId', (req, res) => {
+    const group = req.params.group
+    const message = req.params.message
+    const user = req.params.userId
+    let foundGroup = false
+    let foundMessage = false
+    let foundUser = false
+    data.groups.map((g) => {
+        if(g.groupId===Number(group)){
+            foundGroup = true
+            g.mambers.map((m) => {
+                if(m.id===Number(user)){
+                    foundUser = true
+                }
+            })
+            if(foundUser){
+                g.messages.map((m) => {
+                    if(m.messageId===Number(message)){
+                        foundMessage = true
+                        m.reactions.map((r) => {
+                            if(Number(user)===r.author){
+                                res.json({icon: r.icon})
+                            }
+                        })
+                        res.json(null)
+    }})}}})
+    if(!foundGroup)res.status(401).json({error: "No group with that ID exists"})
+    if(!foundUser)res.status(401).json({error: "No user with that ID is part of this group"})
+    if(!foundMessage)res.status(401).json({error: "No message with that ID exists"})
+})
+DataBase.put('/db/groups/:group/:message/react', (req, res) => {
+    const body = req.body
+    const group = req.params.group
+    const message = req.params.message
+    let foundGroup = false
+    let foundMessage = false
+    let foundUser = false
+    let foundReaction = false
+    data.groups.map((g) => {
+        if(g.groupId===Number(group)){
+            foundGroup = true
+            g.mambers.map((m) => {
+                if(m.id===body.authorId){
+                    foundUser = true
+                }
+            })
+            if(foundUser){
+                g.messages.map((m) => {
+                    if(m.messageId===Number(message)){
+                        foundMessage = true
+                        let newReactions = []
+                        m.reactions.map((r) => {
+                            if(body.authorId===r.author){
+                                foundReaction = true
+                                const newReaction = {
+                                    icon: body.icon,
+                                    author: r.author
+                                }
+                                newReactions.push(newReaction)
+                            }else{
+                                newReactions.push(r)
+                            }
+                        })
+                        m.reactions = newReactions
+                        if(!foundReaction){
+                            res.status(401).json({error: "This user has no reaction to edit"})
+                        }else{
+                            writeFile(data, (error) => {
+                                if(error){
+                                    res.status(404).send("Reaction not Changed")
+                                    return
+                                }
+                                res.send("Reaction Changed")
+    })}}})}}})
+    if(!foundGroup)res.status(401).json({error: "No group with that ID exists"})
+    if(!foundUser)res.status(401).json({error: "No user with that ID is part of this group"})
+    if(!foundMessage)res.status(401).json({error: "No message with that ID exists"})
+})
 DataBase.post('/db/groups/:group/:message/react', (req, res) => {
     const body = req.body
     const group = req.params.group
     const message = req.params.message
     let foundGroup = false
     let foundMessage = false
+    let foundUser = false
     data.groups.map((g) => {
         if(g.groupId===Number(group)){
             foundGroup = true
-            g.messages.map((m) => {
-                if(m.messageId===Number(message)){
-                    foundMessage = true
-                    m.reactions.map((r) => {
-                        if(body.authorId===r.author){
-                            if(body.icon===r.icon){
-                                //delete reaction
-                            }else{
-                                //re-label reaction
-                            }
-                        }
-                    })
-                    const newReaction = {
-                        icon: body.icon,
-                        author: body.authorId
-                    }
-                    m.reactions.push(newReaction)
+            g.mambers.map((m) => {
+                if(m.id===body.authorId){
+                    foundUser = true
                 }
             })
-        }
-    })
+            if(foundUser){
+                g.messages.map((m) => {
+                    if(m.messageId===Number(message)){
+                        m.reactions.map((r) => {
+                            if(body.authorId===r.author){
+                                res.status(401).json({error: "This user already has a reaction to this message"})
+                            }
+                        })
+                        const newReaction = {
+                            icon: body.icon,
+                            author: body.authorId
+                        }
+                        m.reactions.push(newReaction)
+                        writeFile(data, (error) => {
+                            if(error){
+                                res.status(404).send("Reaction not Added")
+                                return
+                            }
+                            res.send("Reaction Added")
+    })}})}}})
     if(!foundGroup)res.status(401).json({error: "No group with that ID exists"})
+    if(!foundUser)res.status(401).json({error: "No user with that ID is part of this group"})
     if(!foundMessage)res.status(401).json({error: "No message with that ID exists"})
-    writeFile(data, (error) => {
-        if(error){
-            res.status(404).send("Message not Sent")
-            return
-        }
-        res.send("Message Sent")
-    })
+})
+DataBase.delete('/db/groups/:group/:message/react', (req, res) => {
+    const body = req.body
+    const group = req.params.group
+    const message = req.params.message
+    let foundGroup = false
+    let foundMessage = false
+    let foundUser = false
+    let foundReaction = false
+    data.groups.map((g) => {
+        if(g.groupId===Number(group)){
+            foundGroup = true
+            g.mambers.map((m) => {
+                if(m.id===body.authorId){
+                    foundUser = true
+                }
+            })
+            if(foundUser){
+                g.messages.map((m) => {
+                    if(m.messageId===Number(message)){
+                        foundMessage = true
+                        let newReactions = []
+                        m.reactions.map((r) => {
+                            if(body.authorId===r.author){
+                                foundReaction = true
+                            }else{
+                                newReactions.push(r)
+                            }
+                        })
+                        m.reactions = newReactions
+                        if(!foundReaction){
+                            res.status(401).json({error: "This user has no reaction to remove"})
+                        }else{
+                            writeFile(data, (error) => {
+                                if(error){
+                                    res.status(404).send("Reaction not Removed")
+                                    return
+                                }
+                                res.send("Reaction Removed")
+    })}}})}}})
+    if(!foundGroup)res.status(401).json({error: "No group with that ID exists"})
+    if(!foundUser)res.status(401).json({error: "No user with that ID is part of this group"})
+    if(!foundMessage)res.status(401).json({error: "No message with that ID exists"})
 })
 
 module.exports = DataBase
--- app/server/calls.md
+++ app/server/calls.md
@@ -5,32 +5,32 @@
         - don't leave a call to this in the finished product
     - returns all the json data
 
-## GET /db/users
+## Users
+
+### GET /db/users
     - development only
         - don't leave a call to this in the finished product
     - returns all the user data for every user
 
-## GET /db/users/:userId
+### GET /db/users/:userId
     - authentication to be added
         - calls to this may need to be modified
     - returns the data for a specific user
 
-## GET /db/groups
-    - development only
-        - don't leave a call to this in the finished product
-    - returns all the group data for every group
-
-## GET /db/groups/:chat
-    - authentication to be added
-        - calls to this may need to be modified
-    - returns the data for a specific group
-
-## GET /db/:userId/groups
+### GET /db/:userId/groups
     - authentication to be added
         - calls to this may need to be modified
     - returns all the data for every group a specific user is in
 
-## POST /db/users
+### PUT
+    - Change Username
+    - to do
+
+### PUT
+    - Change password
+    - to do
+
+### POST /db/users
     - adds a new user to the list of users
     - body must be a JSON object with the format:
     {
@@ -38,7 +38,23 @@
         password: (String)
     }
 
-## POST /db/groups
+### DELETE
+    - User
+    - to do
+
+## Groups
+
+### GET /db/groups
+    - development only
+        - don't leave a call to this in the finished product
+    - returns all the group data for every group
+
+### GET /db/groups/:group
+    - authentication to be added
+        - calls to this may need to be modified
+    - returns the data for a specific group
+
+### POST /db/groups
     - adds a new group to the list of groups
     - body must be a JSON object with the format:
     {
@@ -46,9 +62,19 @@
         founderName: (String)
     }
     - untested
-        - contact me if you have an error with it
 
-## POST /db/groups/:groupId/invite
+### DELETE
+    - Group
+    - to do
+     - contact me if you have an error with it
+
+## Members
+
+### PUT
+    - change user nickname for a group
+    - to do
+
+### POST /db/groups/:groupId/invite
     - adds a new member to a specific group
     - body must be a JSON object with the format:
     {
@@ -58,8 +84,20 @@
     - untested
         - contact me if you have an error with it
 
-## POST /db/groups/:groupId/post
-    - adds a new member to a specific group
+### DELETE
+    - User from a group
+    - to do
+
+## Messages
+
+### GET /db/groups/:group/latest
+    - authentication to be added
+        - calls to this may need to be modified
+    - returns the latest message for a specific group
+    - to do
+
+### POST /db/groups/:groupId/post
+    - adds a new message to a specific group
     - body must be a JSON object with the format:
     {
         content: (String),
@@ -68,14 +106,47 @@
     - untested
         - contact me if you have an error with it
 
-## POST /db/groups/:groupId/:messageId/react
-    - adds a new member to a specific group
+### PUT
+    - Edit messages
+    - to do
+
+### DELETE
+    - Message
+    - to do
+
+## Reactions
+
+### GET /db/groups/:groupId/:messageId/react/:userId
+    - returns the reaction that this user made
+        - null if absent
+    - untested
+        - contact me if you have an error with it
+
+### PUT /db/groups/:groupId/:messageId/react
+    - swaps this user's reaction
+    - body must be a JSON object with the format:
+    {
+        icon: (String), //just send the new reaction
+        authorId: (Int)
+    }
+    - untested
+        - contact me if you have an error with it
+
+### POST /db/groups/:groupId/:messageId/react
+    - adds a reaction
     - body must be a JSON object with the format:
     {
         icon: (String),
         authorId: (Int)
     }
-    - incomplete
-        - will talk about the extent of this feature soon
+    - untested
+        - contact me if you have an error with it
+
+### DELETE /db/groups/:groupId/:messageId/react
+    - deletes a reaction
+    - body must be a JSON object with the format:
+    {
+        authorId: (Int)
+    }
     - untested
         - contact me if you have an error with it
