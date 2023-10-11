9892edb1b68183c48c01af910163225364ac0ef5
HenryTing6639 <yi-heng.ting@students.mq.edu.au>
2023-10-11 16:29:04 +1100

    new changes

--- app/src/components/Setting Left panel/headerLeft.js
+++ app/src/components/Setting Left panel/headerLeft.js
@@ -1,6 +1,7 @@
 import React from "react";
+import {searchOutline} from "ionicons/icons";
 
-function header () {
+function headerLeft () {
     return (
 
         //  {/* -- search box -- */}
@@ -13,8 +14,10 @@ function header () {
        
         <input class="placeholder: italic placeholder:text-slate-400 block bg-white w-full border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-1 sm:text-sm" placeholder="Search" type="text" name="search"/>
         
+        <ion-icon name="search-outline" class="absolute left-7 top-1/2 transform -translate-y-1/2 text-gray-400 text-lg pointer-events-none"></ion-icon>
+       
         </label>
     )
 }
 
-export default header
+export default headerLeft
--- app/src/components/Setting Right panel/headerRight.js
+++ app/src/components/Setting Right panel/headerRight.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function header () {
+function headerRight () {
     return (
 
          <div className="max-w-none w-full max-h-28 grid grid-cols-2 divide-x-1/3 flex-item-centre font-bold text-3xl bg-gray-300 shadow-md">
@@ -11,4 +11,4 @@ function header () {
     )
 }
 
-export default header
+export default headerRight
