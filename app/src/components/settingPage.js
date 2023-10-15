9d05722955ef0f6b645c10f6483f4e518eb260bc
HenryTing6639 <yi-heng.ting@students.mq.edu.au>
2023-10-15 21:18:34 +1100

    move icon to place

--- app/src/components/settingPage.js
+++ app/src/components/settingPage.js
@@ -27,13 +27,15 @@ function settingPage() {
         {/* -- search box -- */}
         <label class="relative block">
             <span class="sr-only">Search</span>
-                <span class="absolute inset-y-0 left-0 flex items-centre pl-2">
+                <span class="absolute inset-y-0 left-0 flex items-centre pl-5">
                     <svg class="h-5 w-5 fill-slate-300" viewbox="0 0 20 20">
                 </svg>
             </span>
         <input class="placeholder: italic placeholder:text-slate-400 block bg-white w-full border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-1 sm:text-sm" placeholder="Search" type="text" name="search"/>
         
         </label>
+
+        <ion-icon name="search-outline" class="absolute left-3 top-16 transform -translate-y-1/2 text-gray-400 text-lg pointer-events-none"></ion-icon>
 
         </div>
         
