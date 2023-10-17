ec80c505790fb266e05051f555385fdca0b1c364
HenryTing6639 <yi-heng.ting@students.mq.edu.au>
2023-10-17 17:45:48 +1100

    testing on setting page

--- app/src/components/Setting Left panel/headerLeft.js
+++ app/src/components/Setting Left panel/headerLeft.js
@@ -1,23 +1,35 @@
-import React from "react";
-import {searchOutline} from "ionicons/icons";
+import React, { useState } from "react";
+import {searchOutline} from "ionicons/icons"
 
-function headerLeft () {
+function HeaderLeft () {
+    
+    const [query, setQuery] = useState("");
+
+    const handleInputChange = (a) => {
+
+        const newQuery = a.target.value;
+        setQuery(setQuery);
+    };
+    
     return (
 
         //  {/* -- search box -- */}
-        <label class="relative block">
-            <span class="sr-only">Search</span>
-                <span class="absolute inset-y-0 left-0 flex items-centre pl-3">
-                    <svg class="h-5 w-5 fill-slate-300" viewbox="0 0 20 20">
-                </svg>
-            </span>
+        <div className="relative block">
+            <input type="text" value={query}
+                onChange={handleInputChange}
+                    placeholder="Seach"
+                        className="pl-10 pr-4 py-2 w-full border rounded-md" />
        
-        <input class="placeholder: italic placeholder:text-slate-400 block bg-white w-full border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-1 sm:text-sm" placeholder="Search" type="text" name="search"/>
         
-        <ion-icon name="search-outline" class="absolute left-7 top-1/2 transform -translate-y-1/2 text-gray-400 text-lg pointer-events-none"></ion-icon>
+        {/* <input class="placeholder: italic 
+            placeholder:text-slate-400 block bg-white w-full border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-1 sm:text-sm" placeholder="Search" type="text" name="search"/>
+         */}
+        <ion-icon name="search-outline" 
+            class="absolute left-7 top-1/2 transform -translate-y-1/2 text-gray-400 text-lg pointer-events-none">    
+                </ion-icon>
        
-        </label>
+        </div>
     )
 }
 
-export default headerLeft
+export default HeaderLeft
--- app/src/components/Setting Right panel/headerRight.js
+++ app/src/components/Setting Right panel/headerRight.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-function headerRight () {
+function HeaderRight () {
     return (
 
          <div className="max-w-none w-full max-h-28 grid grid-cols-2 divide-x-1/3 flex-item-centre font-bold text-3xl bg-gray-300 shadow-md">
             
-            <div>Settings</div>
+            <div> Settings </div>
        
         </div>
     )
 }
 
-export default headerRight
+export default HeaderRight
