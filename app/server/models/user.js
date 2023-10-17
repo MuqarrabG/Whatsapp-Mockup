c26c2a0574adcb5cb34743a781c8708be1c77c89
MuqarrabG <Muqarrab.oz@gmail.com>
2023-10-17 17:05:59 +1100

    Update user.js
    remove commented out code

--- app/server/models/user.js
+++ app/server/models/user.js
@@ -1,29 +1,5 @@
 import mongoose, { Schema } from "mongoose";
 
-//require('dotenv').config({path: './../../.env'})
-// const uri = process.env.MONGODB_URI;
-// const dbName = process.env.MONGODB_DATABASE;
-
-// // Check values before using them in your database connection
-// console.log(`URI: ${uri}`);
-// console.log(`DB Name: ${dbName}`);
-
-// // we want the database connection to happen synchronously so we define
-// // this async function and use await on the connect call
-// const doConnect = async () => {
-//   console.log('connecting to', process.env.MONGODB_URI);
-//   await mongoose.connect(process.env.MONGODB_URI, {
-//           dbName: process.env.MONGODB_DATABASE,
-//           useNewUrlParser: true,
-//           useUnifiedTopology: true
-//         })
-//       .catch((error) => {
-//           console.log('error connecting to MongoDB:', error.message)
-//       })
-//   }
-// // call the connection function
-// doConnect()
-
 const userSchema = new mongoose.Schema(
   {
     username: {
