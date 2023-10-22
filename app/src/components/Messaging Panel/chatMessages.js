ac31247b6014d1951ea6eba111879ebd9bad65cd
MuqarrabG <84062956+MuqarrabG@users.noreply.github.com>
2023-10-23 01:42:21 +1100

    fixed className error

--- app/src/components/Messaging Panel/chatMessages.js
+++ app/src/components/Messaging Panel/chatMessages.js
@@ -7,7 +7,7 @@ function ChatMessages({ messages, currentUser }) {
       <div className="py-2 px-3">
         {messages.map((message, index) =>
           message.authorID.toString() === currentUser.userId.toString() ? (
-            <div key={index} class="flex justify-end mb-2">
+            <div key={index} className="flex justify-end mb-2">
               <div className="rounded py-2 px-3 bg-[#E2F7CB]">
                 <p className="text-sm mt-1">{message.content}</p>
                 <p className="text-right text-xs text-grey-dark mt-1">{` ${new Date(
@@ -19,7 +19,7 @@ function ChatMessages({ messages, currentUser }) {
               </div>
             </div>
           ) : (
-            <div key={index} class="flex mb-2">
+            <div key={index} className="flex mb-2">
               <div className="rounded py-2 px-3 bg-[#F2F2F2]">
                 <p className="text-sm text-teal">{message.author}</p>
                 <p className="text-sm mt-1">
